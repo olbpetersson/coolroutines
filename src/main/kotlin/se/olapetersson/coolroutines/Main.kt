6c16af2dbeda90e07ca0840cf10e71d48aad9ba4
Ola Petersson <ola.petersson@meltwater.com>
2019-02-20 22:46:31 +0100

    suspending main

--- src/main/kotlin/se/olapetersson/coolroutines/Main.kt
+++ src/main/kotlin/se/olapetersson/coolroutines/Main.kt
@@ -3,8 +3,9 @@ package se.olapetersson.coolroutines
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 
-fun main(args: Array<String>) = runBlocking<Unit> {
+suspend fun main() {
 }
 
